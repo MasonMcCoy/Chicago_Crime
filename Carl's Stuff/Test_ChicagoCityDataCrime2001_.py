5971b4d74c642bc964d16a492195444489e7d9c5
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2020-10-26 22:57:20 -0500

    update

--- Carl's Stuff/Test_ChicagoCityDataCrime2001_.py
+++ Carl's Stuff/Test_ChicagoCityDataCrime2001_.py
@@ -13,10 +13,10 @@ results_df = pd.DataFrame.from_records(cityAPI)
 results_df.to_csv('CrimeData.csv')
 print(results_df)
 
-#2001+ data w/example of 5000 limit with block='008xx N MICHIGAN AVE"
+#2001+ data w/example of 10,000 limit with block='008xx N MICHIGAN AVE"
 import requests
 import pandas as pd
-cityAPI = requests.get('https://data.cityofchicago.org/resource/ijzp-q8t2.json?$limit=5000&$$app_token=yCkPkXAsAml5WGe3oM8SdnvpX&block=008XX%20N%20MICHIGAN%20AVE').json()
+cityAPI = requests.get('https://data.cityofchicago.org/resource/ijzp-q8t2.json?$limit=10000&$$app_token=yCkPkXAsAml5WGe3oM8SdnvpX&block=008XX%20N%20MICHIGAN%20AVE').json()
 results_df = pd.DataFrame.from_records(cityAPI)
 results_df.to_csv('CrimeData.csv')
 print(results_df)
